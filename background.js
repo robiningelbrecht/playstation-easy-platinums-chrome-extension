2e52afd23ff456204b1c33ae010579e4d60be14d
robiningelbrecht <robiningelbrecht@users.noreply.github.com>
2023-01-21 17:16:09 +0100

    initial commit

---
+++ background.js
@@ -0,0 +1,50 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  const settings = { dateLastChecked: new Date().toString() };
+  await setChromeStorageSync({ settings: settings });
+});
+
+chrome.tabs.onActivated.addListener(async (activeInfo) => {
+  const response = await fetch(
+    "https://raw.githubusercontent.com/robiningelbrecht/playstation-easy-platinums/master/public/chrome-extension.json"
+  );
+  const json = await response.json();
+
+  const settings = await getChromeStorageSync("settings");
+  const dateLastChecked = new Date(settings.dateLastChecked);
+  const dateLastAddedGame = new Date(json.lastUpdate.date);
+
+  if (dateLastChecked >= dateLastAddedGame) {
+    return;
+  }
+
+  await chrome.action.setBadgeText({
+    text: "!",
+  });
+  await chrome.action.setBadgeBackgroundColor({
+    color: "#e63946",
+  });
+});
+
+const getChromeStorageSync = async function (key) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.get(key, function (value) {
+        resolve(value[key]);
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+};
+
+const setChromeStorageSync = async function (obj) {
+  return new Promise((resolve, reject) => {
+    try {
+      chrome.storage.sync.set(obj, function () {
+        resolve();
+      });
+    } catch (ex) {
+      reject(ex);
+    }
+  });
+};
---
+++ popup.js
@@ -0,0 +1,118 @@
+const response = await fetch(
+  "https://raw.githubusercontent.com/robiningelbrecht/playstation-easy-platinums/master/public/chrome-extension.json"
+);
+
+if (response.status !== 200) {
+  const message = `An error has occured: ${response.status}`;
+  throw new Error(message);
+}
+
+const settings = { dateLastChecked: new Date().toString() };
+await chrome.storage.sync.set({ settings: settings });
+await chrome.action.setBadgeText({
+  text: "",
+});
+
+const json = await response.json();
+const $root = document.querySelector("div.root");
+
+const $h1 = document.querySelector("h1");
+$h1.setAttribute(
+  "title",
+  "Last updated on " +
+    new Intl.DateTimeFormat("en-GB", {
+      weekday: "long",
+      year: "numeric",
+      month: "long",
+      day: "numeric",
+    }).format(new Date(json.lastUpdate.date))
+);
+
+const template = document.createElement("template");
+template.innerHTML = `
+<div class="game">
+<div class="icon">
+    <img alt="icon" />
+</div>
+<div class="content">
+    <div class="main">
+        <div class="title-wrapper">
+            <div class="title"></div>
+            <kbd></kbd>
+        </div>
+        <div class="trophies">
+            <div class="trophy trophy--platinum">
+                <span>1</span>
+            </div>
+            <div class="trophy trophy--gold">
+                <span></span>
+            </div>
+            <div class="trophy trophy--silver">
+                <span></span>
+            </div>
+            <div class="trophy trophy--bronze">
+                <span></span>
+            </div>
+        </div>
+    </div>
+    <div class="meta">
+        <div class="time">
+            <img src="/images/assets/time.svg" alt="time" />
+            <span></span>
+        </div>
+        <div class="price">
+            <img src="/images/assets/price.svg" alt="price" />
+            <span></span>
+        </div>
+        <div class="points">
+            <img src="/images/assets/arrow-up.svg" alt="points" />
+            <span>1350 points</span>
+        </div>
+    </div>
+</div>
+</div>
+`;
+
+$root.innerHTML = "";
+json.games.forEach((game) => {
+  const $game = template.content.cloneNode(true);
+
+  const $icon = $game.querySelector("div.icon img");
+  $icon.setAttribute(
+    "src",
+    "https://github.com/robiningelbrecht/playstation-easy-platinums/raw/master/assets/thumbs/" +
+      game.thumbnail
+  );
+
+  const $platformAndRegion = $game.querySelector("div.main kbd");
+  $platformAndRegion.innerHTML = [game.platform, game.region]
+    .filter((part) => part !== null)
+    .join(" &#9679; ");
+
+  const $title = $game.querySelector("div.main div.title");
+  $title.innerHTML = game.title;
+
+  const $trophyGold = $game.querySelector("div.trophies div.trophy--gold span");
+  $trophyGold.innerHTML = game.trophiesGold;
+
+  const $trophySilver = $game.querySelector(
+    "div.trophies div.trophy--silver span"
+  );
+  $trophySilver.innerHTML = game.trophiesSilver;
+
+  const $trophyBronze = $game.querySelector(
+    "div.trophies div.trophy--bronze span"
+  );
+  $trophyBronze.innerHTML = game.trophiesBronze;
+
+  const $time = $game.querySelector("div.meta div.time span");
+  $time.innerHTML = game.approximateTime + " min";
+
+  const $price = $game.querySelector("div.meta div.price span");
+  $price.innerHTML = game.priceFormatted || "???";
+
+  const $points = $game.querySelector("div.meta div.points span");
+  $points.innerHTML = game.points + " points";
+
+  $root.appendChild($game);
+});
